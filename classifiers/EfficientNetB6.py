6abdf3acffd6b47b233b468a8b8c9bd9f1312af2
karthik-d <karthikdesingu2000@gmail.com>
2022-10-27 00:29:30 +0530

    trainer code fixed

--- classifiers/EfficientNetB6.py
+++ classifiers/EfficientNetB6.py
@@ -1,22 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
+import os
+from pathlib import Path
 
 import tensorflow as tf
 from tensorflow.keras.layers import Dense,Flatten
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.applications import EfficientNetB6
+from keras.models import load_model
+
+from efficientnet.efficientnet.model import EfficientNetB6
 
 # import pathlib
 # root_path = "/content/gdrive/MyDrive/ISIC_DATASETS/ISIC_Datasets/Classification/Segmented_train_120epochs"
 # data_dir = pathlib.Path(root_path)
 
-
-"""**Splitting the data into training and validation**"""
-
 BATCH_SIZE = 64
 IMG_HEIGHT, IMG_WIDTH = (256, 256)
+
+EPOCHS_REFINE = 20
+EPOCHS_TRAIN = 40
+
+MODELS_PATH = os.path.join(
+  os.path.dirname(os.path.abspath(__file__)), 
+  'models',
+  'effnet-b6'
+) 
+
+if not os.path.isdir(MODELS_PATH):
+  Path(MODELS_PATH).mkdir(
+    exist_ok=False,
+    parents=True
+  )
+
+
+"""**Splitting the data into training and validation**"""
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
@@ -61,7 +80,6 @@ model.trainable = False
 
 # take a tensor and compute the average value of all values across the entire matrix for each of the input channels.
 x = layers.GlobalAveragePooling2D(name="avg_pool")(model.output)
-# 
 x = layers.BatchNormalization()(x)
 
 top_dropout_rate = 0.2
@@ -69,52 +87,28 @@ x = layers.Dropout(top_dropout_rate, name="top_dropout")(x)
 outputs = layers.Dense(8, activation="softmax", name="pred")(x)
 
 # Compile
-model = tf.keras.Model(inputs, outputs, name="EfficientNet")
+model = tf.keras.Model(inputs, outputs, name="EfficientNetB6")
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2)
 model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-#20 epochs
+# REFINE
+hist = model.fit(train_ds, epochs=EPOCHS_REFINE, validation_data=val_ds)
 
-epochs = 20
-hist = model.fit(train_ds, epochs=epochs, validation_data=val_ds)
-
-model.save('/content/gdrive/MyDrive/ISIC_DATASETS/ISIC_Datasets/Classification/rms_Processed_Segmented_B2_efficientnet_model.h5')
-print("EfficientNet Model saved")
-
-#40 epochs
-
+# TRAIN
 model.trainable = True
-epochs=20
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
 model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-hist = model.fit(train_ds, epochs=epochs, validation_data=val_ds)
+hist = model.fit(train_ds, epochs=EPOCHS_TRAIN, validation_data=val_ds)
 
-model.save('/content/gdrive/MyDrive/ISIC_DATASETS/ISIC_Datasets/Classification/rms.h5')
-print("EfficientNet Model saved")
 
-#45 epochs
+"""**Validation and Testing**"""
 
-epochs=5
-hist = model.fit(train_ds, epochs=epochs, validation_data=val_ds)
+VAL_DATA_COUNT = len(val_ds)
 
-model.save('/content/gdrive/MyDrive/ISIC_DATASETS/ISIC_Datasets/Classification/Segmented1_efficientnet_45_epochs.h5')
-print("EfficientNet Model saved")
+model = load_model(os.path.join(MODELS_PATH, "effnet-b6", "epoch-001.h5"))
 
-#50 epochs
-
-epochs=5
-hist = model.fit(train_ds, epochs=epochs, validation_data=val_ds)
-
-model.save('/content/gdrive/MyDrive/ISIC_DATASETS/ISIC_Datasets/Classification/Segmented_efficientnet_50_epochs.h5')
-print("EfficientNet Model saved")
-
-"""**Validation Testing**"""
-
-from keras.models import load_model
-model=load_model('/content/gdrive/MyDrive/ISIC_DATASETS/ISIC_Datasets/Classification/Segmented_efficientnet_50_epochs.h5')
-
-y_val = np.zeros(5066, dtype=object)
-x_val = np.zeros([5066, 256, 256, 3])
+y_val = np.zeros(VAL_DATA_COUNT, dtype=object)
+x_val = np.zeros([VAL_DATA_COUNT, 256, 256, 3])
 i=0
 j=0
 
@@ -126,14 +120,12 @@ for x,y in val_ds:
     y_val[i] = p
     i+=1
 
-y_label = np.empty(5066, dtype=object)
+y_label = np.empty(VAL_DATA_COUNT, dtype=object)
 for i in range(len(y_val)):
   y_label[i] = np.array(y_val[i])
 
 classnames = train_ds.class_names
-classnames
-
-pred=model.predict(x_val,BATCH_SIZE =64, verbose=1)
+pred = model.predict(x_val, batch_size=64, verbose=1)
 
 y_true=[]
 for i in range(len(y_label)):
@@ -143,13 +135,6 @@ output_class=[]
 for i in range(len(pred)):
   output_class.append(classnames[np.argmax(pred[i])])
 
-"""**Displaying few of the predicted samples**"""
-
-for i in range(50):
-  print("\nImage : ", i+1)
-  cv2_imshow(x_val[i])
-  print("The actual class is", y_true[i])
-  print("The predicted class is", output_class[i])
 
 Crct_Counter={'AK':0,'MEL':0,'BCC':0,'NV':0,'BKL':0,'DF':0,'VASC':0,'SCC':0}
 Total_Counter={'AK':0,'MEL':0,'BCC':0,'NV':0,'BKL':0,'DF':0,'VASC':0,'SCC':0}
@@ -157,6 +142,7 @@ for i in range(len(pred)):
   Total_Counter[y_true[i]]+=1
   if(y_true[i]==output_class[i]):
     Crct_Counter[y_true[i]]+=1
+
 
 """**Accuracies for each class**"""
 
@@ -170,11 +156,15 @@ print("DF\t\t\t",Crct_Counter['DF'],"\t\t\t ",Total_Counter['DF'],"\t\t\t",round
 print("VASC\t\t\t",Crct_Counter['VASC'],"\t\t\t ",Total_Counter['VASC'],"\t\t\t",round(Crct_Counter['VASC']/Total_Counter['VASC'],2))
 print("SCC\t\t\t",Crct_Counter['SCC'],"\t\t\t ",Total_Counter['SCC'],"\t\t\t",round(Crct_Counter['SCC']/Total_Counter['SCC'],2))
 
+
+
 """**Displaying the classification report**"""
 
 from sklearn.metrics import classification_report
 target_names = ['AK', 'BCC', 'BKL', 'DF', 'MEL', 'NV', 'SCC', 'VASC']
 print(classification_report(y_true, output_class, target_names=target_names))
+
+
 
 """**Displaying the Confusion Matrix**"""
 
